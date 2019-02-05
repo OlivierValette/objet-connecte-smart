a17c9a0be5cec81d4d4620572f8d0157490825bf
tof <cap2family@hotmail.fr>
2019-02-05 12:22:13 +0000

    Css fixing

--- static/app.js
+++ static/app.js
@@ -1,26 +1,26 @@
 $(function () {
 	socket = io.connect('http://' + document.domain + ':' + location.port);
 	socket.on ('connect', function() {
-		$('#status').text('Connecté');
+		$('.redled').attr('class','redled');
+		$('.greenled').attr('class','greenled active');
     	socket.emit('client_connected', {data: 'New client!'});
 	});
 
 	socket.on ('disconnect', function() {
-		$('#status').text('Déconnecté');
+		$('.redled').attr('class','redled active');
+		$('.greenled').attr('class','greenled');
+
 	});
 
 	socket.on ('alert', function (data) {
-    	$('#status').text('Connecté');
         $('#RedAlert').attr("hidden", data);
 	});
 
 	socket.on ('temperature', function (data) {
-    	$('#status').text('Connecté');
         $('#tc').html(data.toString() + " °C");
 	});
 
 	socket.on ('lumen', function (data) {
-		$('#status').text('Connecté');
 		$('#lux').html(data.toString());
 		let bright = Math.max(1 - data/250, 0) * 100;
 		$('.light-ring').css({
